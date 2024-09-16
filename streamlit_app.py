8d7f924ff071db595ff192ec2b9d7f39fb2b31f0
ARKA1112 <bagha92@gmail.com>
2024-09-16 20:58:55 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,7 +53,4 @@ if ingredients_list:
     if time_to_insert:
         session.sql(my_insert_stmt).collect()
         st.success(f'Your Smoothie is ordered! {name_on_order}', icon="✅")
-
-
-
-fv_df = st.dataframe(data=fruityvice_response.json(), use_container_width=True)
+#####################EOL#####################################################
